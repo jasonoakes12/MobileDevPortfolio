22f35020a8d98335942af1fc014f4cfce61bdb53
jasonoakes12 <112090261+jasonoakes12@users.noreply.github.com>
2023-04-18 19:25:28 -0400

    Basic Structure

--- portfolio/App.js
+++ portfolio/App.js
@@ -1,14 +1,47 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const creationShowcase = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button title="Go to Recipe Detail"
+        onPress={() => 
+          navigation.navigate('Details', { name: 'Strawberry Daiquiri' })} />
+      <Button title="Add a Recipe"
+        onPress={() => 
+          navigation.navigate('Add')} />
     </View>
-  );
+  )
 }
+
+const creationDetail = ({navigation, route}) => {
+  return <Text>Recipe for {route.params.name}</Text>
+}
+
+const addCreation = ({navigation, route}) => {
+  return (
+    <Text>Add Creation Screen</Text>
+  )
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Showcase">
+        <Stack.Screen name="Showcase" component={creationShowcase} options={{ title: 'Cocktail Recipes'}}/>
+        <Stack.Screen name="Details" component={creationDetail} options={{ title: 'Recipe Details'}}/>
+        <Stack.Screen name="Add" component={addCreation} options={{ title: 'Add a Recipe'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
